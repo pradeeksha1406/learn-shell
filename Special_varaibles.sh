a4342077434b29b7bf87bad5e8571a778eb12918
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-11 14:56:28 +0530

    if else condition

--- Special_varaibles.sh
+++ Special_varaibles.sh
@@ -1,8 +1,8 @@
 # Special variables are  0-n , * , @ , # ,?
 
-echo scrpit name -$0
-echo first argument -$1
+echo scrpit name - $0
+echo first argument - $1
 echo second Arguement - $2
-echo all argument -$@
+echo all argument - $@
 echo all arguement - $*
 echo NO of Arguement -$#
