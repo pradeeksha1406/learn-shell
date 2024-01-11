8b8fed8477a41d89083829cfdb899b36b1c1bade
Pradeep <Pradeepgupta.abes@gmail.com>
2024-01-11 14:53:03 +0530

    if else condition

---
+++ colorprint.sh
@@ -0,0 +1,11 @@
+echo hello
+echo -e "\e[31mRED COLOR\e[0m"
+#syntax of color print
+
+#echo -e "e\[COLmMESSAGE\[0m"
+
+# -e to enable the color
+# \e\COLM  -To define which color
+#\e[0m - to disable color
+
+
